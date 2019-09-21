7c2a0b8f24c203fe2dcff58eaf03cc11690c47c6
Darth9 <reyprimerjefe@gmail.com>
2019-09-21 07:01:06 +0100

    Update blobs for Android 10

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ TARGET_USES_UEFI := true
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno640
+PLATFORM_VERSION := 10.0.0
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xa90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive
