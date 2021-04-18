27bc46b5e14ceb2524e78b8288cf9e8c03b42145
Darth9 <reyprimerjefe@gmail.com>
2021-04-18 17:06:32 +0100

    Add stock kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2019 The TwrpBuilder Open-Source Project
 #
-# Copyright (C) 2019-2020 OrangeFox Recovery Project
+# Copyright (C) 2019-2021 OrangeFox Recovery Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -60,6 +60,10 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 else
    TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
+   # whether to use stock kernel (from global_images_V12.0.5.0.QFKMIXM_20210224)
+   ifeq ($(FOX_USE_STOCK_KERNEL),1)
+     TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image-stock.gz-dtb
+   endif
 PRODUCT_COPY_FILES += \
     $(TARGET_PREBUILT_KERNEL):kernel
 endif
