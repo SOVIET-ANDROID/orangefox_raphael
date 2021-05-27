2474ba8ee4831d87d9ce0cc7390ee16305d223b0
Darth9 <reyprimerjefe@gmail.com>
2021-05-27 13:51:01 +0100

    Use lzma compression

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_FBE := true
 
+# lzma
+LZMA_RAMDISK_TARGETS := recovery
+
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
